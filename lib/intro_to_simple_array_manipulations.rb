b48c5c437e8d3ed495ad15179e65b3f2a399af5a
witty-banner-1950 <asimsali95@gmail.com>
2019-05-23 18:08:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,8 +28,8 @@ def using_concat (array1, array2)
   array1.concat(array2)
 end
 
-def using_insert (list_of_esoteric_programming_languages, another_esoteric_language)
-  list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
+def using_insert (array, element)
+  array.insert(4, element)
 end
 
 def using_uniq (captain_planet_and_the_planeteers)
